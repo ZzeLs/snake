b61ecde33276bcab1d8242b7f220f9182ad8b56b
ZzeLs <zzels@mail.ru>
2022-04-13 18:05:06 +0500

    Add Class point

---
+++ snake/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    public class Point
+    {
+        public int x = 1;
+        public int y = 3;
+        public char sym;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
